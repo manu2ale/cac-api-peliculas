bc91e57b1274ceb6cccb83c4146d43ade9d34f95
Ale Llampa <manu.ale.llampa@gmail.com>
2024-07-12 14:38:37 -0300

    API Java 1.0

---
+++ src/main/java/com/pelis/Pelicula.java
@@ -0,0 +1,59 @@
+package com.pelis;
+
+public class Pelicula {
+    private int idPelicula;
+    private String titulo;
+    private String genero;
+    private String duracion;
+    private String imagen;
+
+    public Pelicula() {}
+
+    public Pelicula(int idPelicula,String titulo, String genero, String duracion, String imagen) {
+        this.idPelicula = idPelicula;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.imagen = imagen;
+    }
+
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
